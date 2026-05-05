e372473d4f3cea95dec6ef6e2291a8f5afe5531f
Jean-Marc Berthommé <jean-marc.berthomme@u-bordeaux.fr>
2026-05-05 14:27:35 +0200

    Add 'stepper2' motor for azimuth

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,69 @@
 #include <Arduino.h>
 #include <AccelStepper.h>
 
-#define DIR_PIN 2
-#define STEP_PIN 3
-#define ENABLE_PIN 4
+#define DIR_PIN1 2
+#define STEP_PIN1 3
+#define ENABLE_PIN1 4
+
+#define DIR_PIN2 7
+#define STEP_PIN2 6
+#define ENABLE_PIN2 5
 
 // Driver mode (STEP + DIR)
-AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
+AccelStepper stepper1(AccelStepper::DRIVER, STEP_PIN1, DIR_PIN1);
+AccelStepper stepper2(AccelStepper::DRIVER, STEP_PIN2, DIR_PIN2);
 
 void setup() {
-  pinMode(ENABLE_PIN, OUTPUT);
-  digitalWrite(ENABLE_PIN, LOW); // Enable driver
+  pinMode(ENABLE_PIN1, OUTPUT);
+  digitalWrite(ENABLE_PIN1, LOW); // Enable driver
+  pinMode(ENABLE_PIN2, OUTPUT);
+  digitalWrite(ENABLE_PIN2, LOW); // Enable driver
 
   Serial.begin(9600);
   delay(1000);
 
-  Serial.println("Stepper Motor Test");
+  Serial.println("Stepper Motors 1&2 Test");
 
-  // Réglages SAFE pour commencer
-  stepper.setMaxSpeed(1000);        // steps/sec   : 300 -> 1000
-  stepper.setAcceleration(500);    // steps/sec^2 : 100 -> 500
+  stepper1.setMaxSpeed(1000);       // steps/sec   : 300 -> 1000
+  stepper1.setAcceleration(500);    // steps/sec^2 : 100 -> 500
+  stepper1.setCurrentPosition(0);   // reset position
 
-  stepper.setCurrentPosition(0);   // reset position
+  stepper2.setMaxSpeed(300);        // steps/sec   : 300 -> 1000
+  stepper2.setAcceleration(100);    // steps/sec^2 : 100 -> 500
+  stepper2.setCurrentPosition(0);   // reset position
 }
 
 void loop() {
-  const int target = 4000; // mouvement visible
+  const int target1 = 4000;
+  const int target2 =  200;
 
   // Aller
-  Serial.println("Moving to target...");
-  stepper.moveTo(target);
-
-  while (stepper.distanceToGo() != 0) {
-    stepper.run();
+  Serial.println("Moving to target1...");
+  stepper1.moveTo(target1);
+  while (stepper1.distanceToGo() != 0) {
+    stepper1.run();
   }
 
-  Serial.println("Target reached");
+  Serial.println("Moving to target2...");
+  stepper2.moveTo(target2);
+  while (stepper2.distanceToGo() != 0) {
+    stepper2.run();
+  }
+
   delay(1000);
 
   // Retour
-  Serial.println("Returning to zero...");
-  stepper.moveTo(0);
-
-  while (stepper.distanceToGo() != 0) {
-    stepper.run();
+  Serial.println("Returning to zero1...");
+  stepper1.moveTo(0);
+  while (stepper1.distanceToGo() != 0) {
+    stepper1.run();
   }
 
-  Serial.println("Back to zero");
+  Serial.println("Returning to zero2...");
+  stepper2.moveTo(0);
+  while (stepper2.distanceToGo() != 0) {
+    stepper2.run();
+  }
+
   delay(1000);
 }
